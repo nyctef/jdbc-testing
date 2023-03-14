9ed3381c48e86a522f9e30bf82a1e8d873ab61a1
Nyctef <nyctef@nyctef.com>
2023-03-14 15:48:29 +0000

    get a useful test verifying behavior of trustServerCertificate property

--- src/test/java/hello/SqlServerJdbcTest.java
+++ src/test/java/hello/SqlServerJdbcTest.java
@@ -1,20 +1,36 @@
 package hello;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import java.sql.DriverPropertyInfo;
+import java.util.stream.Stream;
 
 import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 
 public class SqlServerJdbcTest {
-    @Test
-    public void acceptsBasicJdbcUrl() throws Exception {
-      assertTrue(new SQLServerDriver().acceptsURL("jdbc:sqlserver://localhost;databaseName=database"));
-    }
-  
-    @Test
-    public void rejectsBasicUnrecognisedUrl() throws Exception {
-      assertFalse(new SQLServerDriver().acceptsURL("jdbc:somethingelse:asdfasdf"));
-    }
+  @Test
+  public void acceptsBasicJdbcUrl() throws Exception {
+    assertTrue(new SQLServerDriver().acceptsURL("jdbc:sqlserver://localhost;databaseName=database"));
+  }
 
+  @Test
+  public void rejectsBasicUnrecognisedUrl() throws Exception {
+    assertFalse(new SQLServerDriver().acceptsURL("jdbc:somethingelse:asdfasdf"));
+  }
+
+  @Test
+  public void trustServerCertificateRequiresValue() throws Exception {
+    DriverPropertyInfo[] props = new SQLServerDriver()
+        .getPropertyInfo("jdbc:sqlserver://localhost;trustServerCertificate=", null);
+    DriverPropertyInfo prop = Stream.of(props).filter(x -> x.name == "trustServerCertificate").findFirst().get();
+    assertArrayEquals(new String[] { "true", "false" }, prop.choices);
+
+    Exception ex = assertThrows(
+        Exception.class,
+        () -> new SQLServerDriver().connect("jdbc:sqlserver://localhost;trustServerCertificate=", null));
+    assertEquals(
+        "The property trustServerCertificate does not contain a valid boolean value. Only true or false can be used.",
+        ex.getMessage());
+  }
 }
