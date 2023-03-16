fdcf97571d51178a7eda915b036a0ceea24b7b7e
Sam Blackburn <sam.blackburn@red-gate.com>
2023-03-16 13:46:14 +0000

    Try to connect to a nonexistent server

--- src/test/java/hello/MySqlJdbcTest.java
+++ src/test/java/hello/MySqlJdbcTest.java
@@ -1,12 +1,12 @@
 package hello;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
 
 import java.sql.DriverPropertyInfo;
 import java.util.stream.Stream;
 
-import com.mysql.cj.jdbc.*;
+import org.junit.jupiter.api.Test;
+import com.mysql.cj.jdbc.Driver;
 
 public class MySqlJdbcTest {
   @Test
@@ -17,5 +17,15 @@ public class MySqlJdbcTest {
   @Test
   public void rejectsBasicUnrecognisedUrl() throws Exception {
     assertFalse(new Driver().acceptsURL("jdbc:somethingelse:asdfasdf"));
+  }
+
+  @Test
+  public void CannotConnectToBlah() throws Exception {
+    Exception ex = assertThrows(
+        Exception.class,
+        () -> new Driver().connect("jdbc:mysql://blah", null));
+    assertEquals(
+        "Communications link failure\n\nThe last packet sent successfully to the server was 0 milliseconds ago. The driver has not received any packets from the server.",
+        ex.getMessage());
   }
 }
