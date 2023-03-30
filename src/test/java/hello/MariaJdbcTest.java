3402b91d8f840eb30c95b93f74c62a6b8e06bd2a
Mark Jordan <mark.jordan@red-gate.com>
2023-03-30 10:52:37 +0100

    another mariadb test for using jdbc subprotocols

--- src/test/java/hello/MariaJdbcTest.java
+++ src/test/java/hello/MariaJdbcTest.java
@@ -1,6 +1,8 @@
 package hello;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -34,5 +36,15 @@ public class MariaJdbcTest {
 
         assertThrows(SQLException.class, () -> Configuration
                 .parse("jdbc:mariadb://localhost/db?useServerPrepStmts=not_a_bool").useServerPrepStmts());
+    }
+
+    @Test
+    public void acceptsUrlsWithMysqlSubprotocolIffExtraPropertySpecified() throws Exception {
+        assertNotNull(Configuration.parse("jdbc:mysql://localhost/db?permitMysqlScheme"));
+        assertNotNull(Configuration.parse("jdbc:mysql://localhost/db?permitMysqlScheme=true"));
+        // not 100% sure why we get a null rather than an exception in this case - seems like
+        // something to do with the jdbc framework using the first registered driver that returns
+        // a non-null connection?
+        assertNull(Configuration.parse("jdbc:mysql://localhost/db?"));
     }
 }
