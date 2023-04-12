06e6abca114a3eeef05c06de51fa6369d624398a
Nyctef <nyctef@nyctef.com>
2023-04-12 12:09:19 +0100

    mysql tests for some parameter behavior

--- src/test/java/hello/MySqlJdbcTest.java
+++ src/test/java/hello/MySqlJdbcTest.java
@@ -2,6 +2,9 @@ package hello;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import com.mysql.cj.jdbc.Driver;
 
 public class MySqlJdbcTest {
@@ -15,13 +18,48 @@ public class MySqlJdbcTest {
     assertFalse(new Driver().acceptsURL("jdbc:somethingelse:asdfasdf"));
   }
 
+  // TODO: is there a nicer API we can use that doesn't involve actually trying to
+  // connect to a database?
+
   @Test
-  public void CannotConnectToBlah() throws Exception {
+  public void cannotConnectToBlah() throws Exception {
     Exception ex = assertThrows(
         Exception.class,
         () -> new Driver().connect("jdbc:mysql://blah", null));
     assertEquals(
         "Communications link failure\n\nThe last packet sent successfully to the server was 0 milliseconds ago. The driver has not received any packets from the server.",
         ex.getMessage());
+  }
+
+  @Test
+  public void invalidSslModeValue() throws Exception {
+    Exception ex = assertThrows(
+        Exception.class,
+        () -> new Driver().connect("jdbc:mysql://blah?sslMode=asdf", null));
+    assertEquals(
+        "The connection property 'sslMode' acceptable values are: 'DISABLED', 'PREFERRED', 'REQUIRED', 'VERIFY_CA' or 'VERIFY_IDENTITY'. The value 'asdf' is not acceptable.",
+        ex.getMessage());
+  }
+
+  @Test
+  @ParameterizedTest
+  @ValueSource(strings = { "=asdf", "=", "", "=1", "=0"})
+  public void boolValuesThrowErrorIfInvalid(String paranoidValue) throws Exception {
+    Exception ex = assertThrows(
+        Exception.class,
+        () -> new Driver().connect("jdbc:mysql://blah?paranoid" + paranoidValue, null));
+    assertTrue(
+        ex.getMessage().contains("The connection property 'paranoid' acceptable values are: 'TRUE', 'FALSE', 'YES' or 'NO'. The value '"));
+  }
+
+  @Test
+  @ParameterizedTest
+  @ValueSource(strings = { "=true", "=TRUE", "=false", "=yes", "=no"})
+  public void boolValuesTriesToConnectIfValid(String paranoidValue) throws Exception {
+    Exception ex = assertThrows(
+        Exception.class,
+        () -> new Driver().connect("jdbc:mysql://blah?paranoid" + paranoidValue, null));
+    assertTrue(
+        ex.getMessage().contains("Communications link failure"));
   }
 }
